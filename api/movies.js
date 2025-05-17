cab3dcfab23a3eacaacb517e9cf425b121642079
mahmoudadel810 <mahmoudadel810@gmail.com>
2025-05-17 17:06:32 +0300

    fixed vercel

--- api/movies.js
+++ api/movies.js
@@ -1,5 +1,12 @@
 // Import your data
-import data from '../data.json';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dataPath = path.join(__dirname, '../data.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
 export default function handler(req, res)
 {
---
+++ index.js
@@ -0,0 +1,2 @@
+// Re-export the movies handler as the default handler
+export { default } from './api/movies';
